9451110e9969307b40ea2de1c80cabdb9ee61d7b
Muhammad Zidan Helmy <mzidanhelmyy@gmail.com>
2024-12-14 09:54:06 +0800

    front end index || unclear

---
+++ resources/views/front/index.blade.php
@@ -0,0 +1,426 @@
+	<!DOCTYPE html>
+	<html lang="zxx" class="no-js">
+	<head>
+		<!-- Mobile Specific Meta -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Favicon-->
+		<link rel="shortcut icon" href="{{ asset('img/fav.png') }}">
+		<!-- Author Meta -->
+		<meta name="author" content="codepixer">
+		<!-- Meta Description -->
+		<meta name="description" content="">
+		<!-- Meta Keyword -->
+		<meta name="keywords" content="">
+		<!-- meta character set -->
+		<meta charset="UTF-8">
+		<!-- Site Title -->
+		<title>Art Museum</title>
+
+		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet"> 
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+        {{-- <link rel="stylesheet" href="https://cdn.lineicons.com/1.0/lineicons.css"> --}}
+        {{-- <link href="https://cdn.jsdelivr.net/npm/linearicons@1.0.0/dist/webfont.css" rel="stylesheet"> --}}
+			<!--
+			CSS
+			============================================= -->
+            <link rel="stylesheet" href="{{ asset('css/linearicons.css') }}">
+            {{-- <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}"> --}}
+            <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+            <link rel="stylesheet" href="{{ asset('css/magnific-popup.css') }}">
+            <link rel="stylesheet" href="{{ asset('css/nice-select.css') }}">					
+            <link rel="stylesheet" href="{{ asset('css/animate.min.css') }}">
+            <link rel="stylesheet" href="{{ asset('css/owl.carousel.css') }}">
+            <link rel="stylesheet" href="{{ asset('css/main.css') }}">            
+		</head>
+		<body>
+
+			  <header id="header" id="home">
+			    <div class="container">
+			    	<div class="row align-items-center justify-content-between d-flex mt-2">
+				      <div id="logo">
+				        <a href="index.html"><img src="{{ asset('img/logo.png') }}" alt="" title="" /></a>
+				      </div>
+				      <nav id="nav-menu-container">
+				        <ul class="nav-menu">
+				          <li class="menu-active"><a href="index.html">Home</a></li>
+				          <li><a href="about.html">About</a></li>
+				          <li><a href="gallery.html">Gallery</a></li>
+				          <li><a href="event.html">Events</a></li>
+				          <li><a href="ticket.html">Ticket</a></li>
+				          <li><a href="blog-home.html">Blog</a></li>
+				          <li><a href="contact.html">Contact</a></li>
+				          <li class="menu-has-children"><a href="">Pages</a>
+				            <ul>
+				              <li><a href="blog-single.html">Blog Single</a></li>
+				              <li><a href="category.html">Category</a></li>
+				              <li><a href="elements.html">Elements</a></li>
+				            </ul>
+				          </li>			          
+				        </ul>
+				      </nav><!-- #nav-menu-container -->		    		
+			    	</div>
+			    </div>
+			  </header><!-- #header -->
+
+
+			<!-- start banner Area -->
+			<section class="banner-area relative" id="home">
+				<div class="overlay overlay-bg"></div>	
+				<div class="container">
+					<div class="row fullscreen d-flex align-items-center justify-content-center">
+						<div class="banner-content col-lg-8">
+							<h6 class="text-white">Openning on 21st February, 2018</h6>
+							<h1 class="text-white">
+								Exhibition on Modern Era				
+							</h1>
+							<p class="pt-20 pb-20 text-white">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.  sed do eiusmod tempor incididunt..
+							</p>
+							<a href="#" class="primary-btn text-uppercase">Get Started</a>
+						</div>											
+					</div>
+				</div>					
+			</section>
+			<!-- End banner Area -->	
+
+			<!-- Start service Area -->
+			<section class="service-area pt-100" id="about">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-4">
+							<div class="single-service">
+                                <span class="lnr lnr-clock"></span>
+							  <h4>Openning Hours</h4>
+							  <p>
+							  	Mon - Fri: 10.00am to 05.00pm
+								Sat: 12.00pm to 03.00 pm
+								Sunday Closed
+							  </p>						 	
+							  <div class="overlay">
+							    <div class="text">
+							    	<p>
+							    		Here, I focus on a range of items and features that we use in life Here, I focus on a range of items and features that we use in life Here, I focus on a range of items and features.that we use in life
+							    	</p>
+							    	<a href="#" class="text-uppercase primary-btn">Buy ticket</a>
+							    </div>
+							  </div>
+							</div>							
+						</div>
+						<div class="col-lg-4">
+							<div class="single-service">
+							  <span class="lnr lnr-rocket"></span>
+							  <h4>Ongoing Exhibitions</h4>
+							  <p>
+							  	Mon - Fri: 10.00am to 05.00pm
+								Sat: 12.00pm to 03.00 pm
+								Sunday Closed
+							  </p>						 	
+							  <div class="overlay">
+							    <div class="text">
+							    	<p>
+							    		Here, I focus on a range of items and features that we use in life Here, I focus on a range of items and features that we use in life Here, I focus on a range of items and features.that we use in life
+							    	</p>
+							    	<a href="#" class="text-uppercase primary-btn">Buy ticket</a>
+							    </div>
+							  </div>
+							</div>							
+						</div>
+						<div class="col-lg-4">
+							<div class="single-service">
+							  <span class="lnr lnr-briefcase"></span>
+							  <h4>Openning Events</h4>
+							  <p>
+							  	Mon - Fri: 10.00am to 05.00pm
+								Sat: 12.00pm to 03.00 pm
+								Sunday Closed
+							  </p>						 	
+							  <div class="overlay">
+							    <div class="text">
+							    	<p>
+							    		Here, I focus on a range of items and features that we use in life Here, I focus on a range of items and features that we use in life Here, I focus on a range of items and features.that we use in life
+							    	</p>
+							    	<a href="#" class="text-uppercase primary-btn">Buy ticket</a>
+							    </div>
+							  </div>
+							</div>							
+						</div>												
+					</div>
+				</div>	
+			</section>
+			<!-- End service Area -->
+			
+			<!-- Start quote Area -->
+			<section class="quote-area section-gap">
+				<div class="container">				
+					<div class="row">
+						<div class="col-lg-6 quote-left">
+							<h1>
+								<span>Music</span> gives soul to the universe, <br>
+								wings to the <span>mind</span>, flight <br>
+								to the <span>imagination</span>.
+							</h1>
+						</div>
+						<div class="col-lg-6 quote-right">
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore  et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+							</p>
+						</div>
+					</div>
+				</div>	
+			</section>
+			<!-- End quote Area -->
+
+			<!-- Start exibition Area -->
+			<section class="exibition-area section-gap" id="exhibitions">
+				<div class="container">
+					<div class="row d-flex justify-content-center">
+						<div class="menu-content pb-60 col-lg-10">
+							<div class="title text-center">
+								<h1 class="mb-10">Ongoing Exhibitions from the scratch</h1>
+								<p>Who are in extremely love with eco friendly system.</p>
+							</div>
+						</div>
+					</div>						
+					<div class="row">
+						<div class="active-exibition-carusel">
+							<div class="single-exibition item">
+								<img src="{{ asset('img/e1.jpg') }}" alt="">
+								<ul class="tags">
+									<li><a href="#">Travel</a></li>
+									<li><a href="#">Life style</a></li>
+								</ul>
+								<a href="#"><h4>Portable latest blog for women</h4></a>
+								<p>
+									Lorem ipsum dolor sit amet, consec teturadip isicing elit, sed do eiusmod tempor.
+								</p>
+								<h6 class="date">31st January, 2018</h6>
+							</div>
+
+							<div class="single-exibition item">
+								<img src="{{ asset('img/e2.jpg') }}" alt="">
+								<ul class="tags">
+									<li><a href="#">Travel</a></li>
+									<li><a href="#">Life style</a></li>
+								</ul>
+								<a href="#"><h4>Portable latest blog for women</h4></a>
+								<p>
+									Lorem ipsum dolor sit amet, consec teturadip isicing elit, sed do eiusmod tempor.
+								</p>
+								<h6 class="date">31st January, 2018</h6>
+							</div>
+
+							<div class="single-exibition item">
+								<img src="{{ asset('img/e3.jpg') }}" alt="">
+								<ul class="tags">
+									<li><a href="#">Travel</a></li>
+									<li><a href="#">Life style</a></li>
+								</ul>
+								<a href="#"><h4>Portable latest blog for women</h4></a>
+								<p>
+									Lorem ipsum dolor sit amet, consec teturadip isicing elit, sed do eiusmod tempor.
+								</p>
+								<h6 class="date">31st January, 2018</h6>
+							</div>							
+							<div class="single-exibition item">
+								<img src="{{ asset('img/e1.jpg') }}" alt="">
+								<ul class="tags">
+									<li><a href="#">Travel</a></li>
+									<li><a href="#">Life style</a></li>
+								</ul>
+								<a href="#"><h4>Portable latest blog for women</h4></a>
+								<p>
+									Lorem ipsum dolor sit amet, consec teturadip isicing elit, sed do eiusmod tempor.
+								</p>
+								<h6 class="date">31st January, 2018</h6>
+							</div>
+
+							<div class="single-exibition item">
+								<img src="{{ asset('img/e2.jpg') }}" alt="">
+								<ul class="tags">
+									<li><a href="#">Travel</a></li>
+									<li><a href="#">Life style</a></li>
+								</ul>
+								<a href="#"><h4>Portable latest blog for women</h4></a>
+								<p>
+									Lorem ipsum dolor sit amet, consec teturadip isicing elit, sed do eiusmod tempor.
+								</p>
+								<h6 class="date">31st January, 2018</h6>
+							</div>
+
+							<div class="single-exibition item">
+								<img src="{{ asset('img/e3.jpg') }}" alt="">
+								<ul class="tags">
+									<li><a href="#">Travel</a></li>
+									<li><a href="#">Life style</a></li>
+								</ul>
+								<a href="#"><h4>Portable latest blog for women</h4></a>
+								<p>
+									Lorem ipsum dolor sit amet, consec teturadip isicing elit, sed do eiusmod tempor.
+								</p>
+								<h6 class="date">31st January, 2018</h6>
+							</div>							
+							<div class="single-exibition item">
+								<img src="{{ asset('img/e1.jpg') }}" alt="">
+								<ul class="tags">
+									<li><a href="#">Travel</a></li>
+									<li><a href="#">Life style</a></li>
+								</ul>
+								<a href="#"><h4>Portable latest blog for women</h4></a>
+								<p>
+									Lorem ipsum dolor sit amet, consec teturadip isicing elit, sed do eiusmod tempor.
+								</p>
+								<h6 class="date">31st January, 2018</h6>
+							</div>
+
+							<div class="single-exibition item">
+								<img class="img-fluid" src="{{ asset('img/e2.jpg') }}" alt="">
+								<ul class="tags">
+									<li><a href="#">Travel</a></li>
+									<li><a href="#">Life style</a></li>
+								</ul>
+								<a href="#"><h4>Portable latest blog for women</h4></a>
+								<p>
+									Lorem ipsum dolor sit amet, consec teturadip isicing elit, sed do eiusmod tempor.
+								</p>
+								<h6 class="date">31st January, 2018</h6>
+							</div>
+
+							<div class="single-exibition item">
+								<img class="img-fluid" src="{{ asset('img/e3.jpg') }}" alt="">
+								<ul class="tags">
+									<li><a href="#">Travel</a></li>
+									<li><a href="#">Life style</a></li>
+								</ul>
+								<a href="#"><h4>Portable latest blog for women</h4></a>
+								<p>
+									Lorem ipsum dolor sit amet, consec teturadip isicing elit, sed do eiusmod tempor.
+								</p>
+								<h6 class="date">31st January, 2018</h6>
+							</div>
+						</div>													
+					</div>
+				</div>	
+			</section>
+			<!-- End exibition Area -->			
+		
+			<!-- Start upcoming-event Area -->
+			<section class="upcoming-event-area section-gap" id="events">
+				<div class="container">
+					<div class="row d-flex justify-content-center">
+						<div class="menu-content pb-60 col-lg-10">
+							<div class="title text-center">
+								<h1 class="mb-10">Checkout our Upcoming Events</h1>
+								<p>Who are in extremely love with eco friendly system.</p>
+							</div>
+						</div>
+					</div>						
+					<div class="row">
+						<div class="col-lg-6 event-left">
+							<div class="single-events">
+								<img class="img-fluid" src="{{ asset('img/u1.jpg') }}" alt="">
+								<a href="#"><h4>Event on the rock solid carbon</h4></a>
+								<h6><span>21st February</span> at Central government museum</h6>
+								<p>
+									inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially.
+								</p>
+								<a href="#" class="primary-btn text-uppercase">View Details</a>
+							</div>
+						</div>
+						<div class="col-lg-6 event-right">
+							<div class="single-events">
+								<a href="#"><h4>Event on the rock solid carbon</h4></a>
+								<h6><span>21st February</span> at Central government museum</h6>
+								<p>
+									inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially.
+								</p>
+								<a href="#" class="primary-btn text-uppercase">View Details</a>
+								<img class="img-fluid" src="{{ asset('img/u2.jpg') }}" alt="">
+							</div>
+						</div>
+					</div>
+				</div>	
+			</section>
+			<!-- End upcoming-event Area -->
+
+			<!-- start footer Area -->		
+			<footer class="footer-area section-gap" style="background: #343a40; color: #ffffff; padding: 20px 0;">
+                <div class="container">
+                    <div class="row">
+                        <!-- About Us -->
+                        <div class="col-lg-5 col-md-6 col-sm-6">
+                            <div class="single-footer-widget">
+                                <h6 style="color: white; margin-bottom: 15px; font-weight: bold;">About Us</h6>
+                                <p style="line-height: 1.8; margin-bottom: 15px;">
+                                    Sistem Informasi Museum adalah platform digital yang menyediakan informasi lengkap 
+                                    tentang koleksi museum kami. Kami berkomitmen untuk melestarikan sejarah 
+                                    dan memberikan edukasi kepada pengunjung.
+                                </p>
+                                <p class="footer-text" style="font-size: 14px; color: #ccc;">
+                                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a> and distributed by <a href="https://themewagon.com" target="_blank">ThemeWagon</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                                </p>
+                            </div>
+                        </div>
+            
+                        <!-- Contact Us -->
+                        <div class="col-lg-5 col-md-6 col-sm-6">
+                            <div class="single-footer-widget">
+                                <h6 style="color: white; margin-bottom: 15px; font-weight: bold;">Contact Us</h6>
+                                <p style="line-height: 1.8; margin-bottom: 10px;">Untuk pertanyaan lebih lanjut, hubungi kami:</p>
+                                <ul style="list-style: none; padding: 0; margin: 0;">
+                                    <li style="margin-bottom: 10px; color: #ffffff;">
+                                        <i class="fa fa-map-marker" style="margin-right: 10px; color: #ff9900;"></i> 
+                                        Jl. Museum No. 12, Jakarta
+                                    </li>
+                                    <li style="margin-bottom: 10px; color: #ffffff;">
+                                        <i class="fa fa-envelope" style="margin-right: 10px; color: #ff9900;"></i> 
+                                        info@museum.com
+                                    </li>
+                                    <li style="margin-bottom: 10px; color: #ffffff;">
+                                        <i class="fa fa-phone" style="margin-right: 10px; color: #ff9900;"></i> 
+                                        +62 123 456 789
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+            
+                        <!-- Museum Hours -->
+                        <div class="col-lg-2 col-md-6 col-sm-6 social-widget">
+                            <div class="single-footer-widget">
+                                <h6 style="color: white; margin-bottom: 15px; font-weight: bold;">Museum Hours</h6>
+                                <p style="line-height: 1.8; margin: 0;">
+                                    <strong>Senin - Jumat:</strong> 09:00 - 17:00<br>
+                                    <strong>Sabtu - Minggu:</strong> 10:00 - 18:00
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            
+			<!-- End footer Area -->		
+
+			<script src="{{ asset('js/vendor/jquery-2.2.4.min.js') }}"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+            <script src="{{ asset('js/vendor/bootstrap.min.js') }}"></script>
+            <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+            <script src="{{ asset('js/easing.min.js') }}"></script>
+            <script src="{{ asset('js/hoverIntent.js') }}"></script>
+            <script src="{{ asset('js/superfish.min.js') }}"></script>
+            <script src="{{ asset('js/jquery.ajaxchimp.min.js') }}"></script>
+            <script src="{{ asset('js/jquery.magnific-popup.min.js') }}"></script>
+            <script src="{{ asset('js/owl.carousel.min.js') }}"></script>
+            <script src="{{ asset('js/imagesloaded.pkgd.min.js') }}"></script>
+            <script src="{{ asset('js/justified.min.js') }}"></script>
+            <script src="{{ asset('js/jquery.sticky.js') }}"></script>
+            <script src="{{ asset('js/jquery.nice-select.min.js') }}"></script>
+            <script src="{{ asset('js/parallax.min.js') }}"></script>
+            <script src="{{ asset('js/mail-script.js') }}"></script>
+            <script src="{{ asset('js/main.js') }}"></script>
+
+		</body>
+	</html>
+
+
+
