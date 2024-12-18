1fa00854460b807f75558dafd3d40600dbc5a018
Muhammad Zidan Helmy <mzidanhelmyy@gmail.com>
2024-12-18 09:20:44 +0800

    Menghubungkan backend untuk menampilkan gambar koleksi museum di halaman galeri front-end | Connect backend to display museum collection images on the gallery front-end

--- resources/views/front/gallery.blade.php
+++ resources/views/front/gallery.blade.php
@@ -11,7 +11,7 @@
                   <h1 class="text-white">
                       Art Gallery				
                   </h1>	
-                  <p class="text-white link-nav"><a href="index.html">Home </a>  <span class="lnr lnr-arrow-right"></span>  <a href="gallery.html"> Gallery</a></p>
+                  <p class="text-white link-nav"><a href="{{ route('index') }}">Home </a>  <span class="lnr lnr-arrow-right"></span>  <a href="gallery.html"> Gallery</a></p>
               </div>											
           </div>
       </div>
@@ -31,21 +31,13 @@
               </div>
           </div>						
           <div id="grid-container" class="row">
-              <a class="single-gallery" href="img/g1.jpg"><img class="grid-item" src="img/g1.jpg"></a>
-              <a class="single-gallery" href="img/g2.jpg"><img class="grid-item" src="img/g2.jpg"></a>
-              <a class="single-gallery" href="img/g3.jpg"><img class="grid-item" src="img/g3.jpg"></a>
-              <a class="single-gallery" href="img/g4.jpg"><img class="grid-item" src="img/g4.jpg"></a>
-              <a class="single-gallery" href="img/g5.jpg"><img class="grid-item" src="img/g5.jpg"></a>
-              <a class="single-gallery" href="img/g6.jpg"><img class="grid-item" src="img/g6.jpg"></a>
-              <a class="single-gallery" href="img/g7.jpg"><img class="grid-item" src="img/g7.jpg"></a>
-              <a class="single-gallery" href="img/g8.jpg"><img class="grid-item" src="img/g8.jpg"></a>
-              <a class="single-gallery" href="img/g9.jpg"><img class="grid-item" src="img/g9.jpg"></a>
-              <a class="single-gallery" href="img/g10.jpg"><img class="grid-item" src="img/g10.jpg"></a>
-              <a class="single-gallery" href="img/g11.jpg"><img class="grid-item" src="img/g11.jpg"></a>
-              <a class="single-gallery" href="img/g12.jpg"><img class="grid-item" src="img/g12.jpg"></a>
-              <a class="single-gallery" href="img/g4.jpg"><img class="grid-item" src="img/g4.jpg"></a>
-              <a class="single-gallery" href="img/g5.jpg"><img class="grid-item" src="img/g5.jpg"></a>						
-          </div>	
+            @foreach($collections as $collection)
+                <a class="single-gallery" href="{{ asset('storage/' . $collection->image) }}">
+                    <img class="grid-item" src="{{ asset('storage/' . $collection->image) }}">
+                </a>
+            @endforeach
+        </div>
+        
       </div>	
   </section>
   <!-- End gallery Area -->			
